c43cc8e04093eb3e3d9b16759b80815bdf55302a
chr1sadams <37448132+chr1sadams@users.noreply.github.com>
2019-04-10 22:04:48 -0400

    Update Maze.py
    added collision

--- Maze.py
+++ Maze.py
@@ -1,4 +1,4 @@
-﻿# maze.py starter Code
+# maze.py starter Code
 
 import viz
 import vizshape
@@ -19,7 +19,7 @@ class Maze(viz.EventClass):
 		
 		# avatar's x,z location in maze and its rotation angle
 		self.theta = 0
-		self.x = 5
+		self.x = 2
 		self.z = 2
 		self.y = 1
 		self.first = False
@@ -31,10 +31,10 @@ class Maze(viz.EventClass):
 		# Array entries containing a 2 represent 1 x 2 x 1 wall blocks.
 		# Array entries containing a 0 represent 1 x 0.1 x 1 floor blocks.
 		self.maze = []
+		#bottom left
 		self.maze = [[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]] + self.maze
-		self.maze = [[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]] + self.maze # row 8
-		self.maze = [[4,0,0,0,0,0,0,0,1,0,0,0,0,0,0,4,4,0,0,0,0,0,0,0,0,0,0,4]] + self.maze
-		self.maze = [[4,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,4]] + self.maze # row 7
+		self.maze = [[4,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,4]] + self.maze
+		self.maze = [[4,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,4]] + self.maze
 		self.maze = [[4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4]] + self.maze
 		self.maze = [[4,1,1,4,4,0,0,4,4,0,0,4,4,1,1,4,4,0,0,4,4,0,0,4,4,1,1,4]] + self.maze
 		self.maze = [[4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4]] + self.maze
@@ -48,10 +48,9 @@ class Maze(viz.EventClass):
 		self.maze = [[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0,0,0,0,0,4]] + self.maze
 		self.maze = [[4,0,0,0,0,0,0,4,4,0,0,4,4,0,0,4,4,4,0,0,4,0,0,4,4,1,1,4]] + self.maze
 		self.maze = [[4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,4,4,0,0,0,4,0,0,4,4,0,0,4]] + self.maze
-		self.maze = [[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4,4,0,0,0,0,0,0,1,0,0,0,4]] + self.maze
+		self.maze = [[4,0,0,0,4,0,0,0,0,0,0,0,0,0,0,4,4,0,0,0,0,0,0,1,0,0,0,4]] + self.maze
 		self.maze = [[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,4]] + self.maze
 		self.maze = [[4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,4]] + self.maze
-		self.maze = [[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]] + self.maze
 		self.maze = [[4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4]] + self.maze
 		
 		view = viz.MainView
@@ -63,20 +62,22 @@ class Maze(viz.EventClass):
 		# Add +x,+y,+z coordinate axes to scene to help with placing the blocks correctly
 		self.addCoordinateAxes()
 		
+		
+		self.blocks = []
 		# Code to create blocks forming the maze goes here
 		for row in range(0,len(self.maze)):
 			for i in range(0,len(self.maze[row])):
 				#* self.maze[i]
 				if(self.maze[row][i] == 4):
-					block = vizshape.addCube(size = 1.0 , color = viz.GREEN)
+					block = vizshape.addCube(size = 1.0 , color = viz.RED)
 				elif(self.maze[row][i] ==1):
 					block = vizshape.addCube(size = 1.0 , color = viz.BLUE)
 				else:
-					block = vizshape.addCube(size = 1.0 , color = viz.YELLOW)
+					block = vizshape.addCube(size = 1.0 , color = viz.BLACK)
 				m = viz.Matrix()
-				xx = .5
+				xx = 1
 				yy = .5
-				zz = .5
+				zz = 1
 				if(row != 0 ):
 					xx = xx + (row * 1)
 				if(i != 0):
@@ -84,85 +85,128 @@ class Maze(viz.EventClass):
 				m.postTrans(xx,yy,zz)
 				
 				if(self.maze[row][i] == 4):
-					
 					m.postScale(1,4.0,1)
 				elif(self.maze[row][i] == 1):
 					m.postScale(1,1.5,1)	
 				else:
 					m.postScale(1,1,1)
-				block.setMatrix(m)	
+				self.blocks = [[xx,zz]] + self.blocks
+				#print(self.blocks)
+				block.setMatrix(m)
 				
+		#print(self.blocks)		
 		self.avatar = viz.add('vcc_female.cfg')
-		self.theta = 0
+		
 		m = viz.Matrix()
 		m.postTrans(self.x,self.y,self.z)
-		
 		m.postAxisAngle(0,1,0,self.theta)
 		self.avatar.setMatrix(m)
 		
-		############################################HERE##########################
-		if(False):
-			for i in range(0,15):
-				self.avatar = viz.add('vcc_female.cfg')
-				self.theta = 0
-				m = viz.Matrix()
-				m.postTrans(5,.5,2)
-				self.avatar.state(i)
-				m.postAxisAngle(0,1,0,self.theta)
-				self.avatar.setMatrix(m)
-			
 		
 		
 	# Key pressed down event code.
 	
 	def onKeyDown(self,key):
 		
-		
 		if (key == viz.KEY_RIGHT):
-			self.theta = self.theta + 5
+			self.theta = self.theta + 90
+			
 			m = viz.Matrix()
-			
-			
 			m.postAxisAngle(0,1,0,self.theta)
 			m.postTrans(self.x,self.y,self.z)
 			self.avatar.setMatrix(m)
+			if(self.theta == 360):
+				self.theta = 0
+			print(self.theta)
 			
 		if (key == viz.KEY_LEFT):
-			self.theta = self.theta - 5
+			self.theta = self.theta - 90
+			
 			m = viz.Matrix()
-			
-			
 			m.postAxisAngle(0,1,0,self.theta)
 			m.postTrans(self.x,self.y,self.z)
 			self.avatar.setMatrix(m)
+			if(self.theta == -360):
+				self.theta = 0
+			
+		if (key == ' '):
+			self.isValid = False
+			if(self.theta == 90 or self.theta == -270):
+				if(self.maze[int(self.x)][int(self.z-1)] == 1):
+					self.isValid = True
+			elif(self.theta == 0):
+				if(self.maze[int(self.x-1)][int(self.z)] == 1):
+					self.isValid = True
+			elif(self.theta == 270 or self.theta == -90):
+				if(self.maze[int(self.x-2)][int(self.z-1)] == 1):
+					self.isValid = True
+			elif(self.theta == 180 or self.theta == -180):
+				if(self.maze[int(self.x-1)][int(self.z-2)] == 1):
+					self.isValid = True										
+			if(self.isValid):
+				if(self.theta == 90 or self.theta == -270):
+					self.x = self.x + 2
+				elif(self.theta == 0):
+					self.z = self.z + 2
+				elif(self.theta == 270 or self.theta == -90):
+					self.x = self.x - 2
+				elif(self.theta == 180 or self.theta == -180):
+					self.z = self.z - 2
+				m = viz.Matrix()
+				m.postAxisAngle(0,1,0,self.theta)
+				m.postTrans(self.x,self.y,self.z)
+				self.avatar.setMatrix(m)
 			
 		if (key == viz.KEY_DOWN):
-			m = viz.Matrix()
-			self.moveZ = math.cos(math.radians(self.theta))
-			self.moveX = math.sin(math.radians(self.theta))
-			#m.postTrans(self.moveX,self.y,self.moveZ)
-			self.x = self.x - self.moveX
-			self.z = self.z - self.moveZ
-			m.postAxisAngle(0,1,0,self.theta)
-			m.postTrans(self.x,self.y,self.z)
-			self.avatar.setMatrix(m)
-			print(self.x)
-			print(self.z)
-			
+					
+			self.isValid = False
+			if(self.theta == 270 or self.theta == -90):
+				if(self.maze[int(self.x)][int(self.z-1)] == 0):
+					self.isValid = True
+			elif(self.theta == 180 or self.theta == -180):
+				if(self.maze[int(self.x-1)][int(self.z)] == 0):
+					self.isValid = True
+			elif(self.theta == 90 or self.theta == -270):
+				if(self.maze[int(self.x-2)][int(self.z-1)] == 0):
+					self.isValid = True
+			elif(self.theta == 0):
+				if(self.maze[int(self.x-1)][int(self.z-2)] == 0):
+					self.isValid = True										
+			if(self.isValid):
+				m = viz.Matrix()
+				self.moveZ = math.cos(math.radians(self.theta))
+				self.moveX = math.sin(math.radians(self.theta))				
+				self.x = self.x - self.moveX
+				self.z = self.z - self.moveZ				
+				m.postAxisAngle(0,1,0,self.theta)
+				m.postTrans(self.x,self.y,self.z)
+				self.avatar.setMatrix(m)
 			
 		if (key == viz.KEY_UP):
-			m = viz.Matrix()
-			self.moveZ = math.cos(math.radians(self.theta))
-			self.moveX = math.sin(math.radians(self.theta))
-			#m.postTrans(self.moveX,self.y,self.moveZ)
-			self.x = self.x + self.moveX
-			self.z = self.z + self.moveZ
-			m.postAxisAngle(0,1,0,self.theta)
-			m.postTrans(self.x,self.y,self.z)
-			self.avatar.setMatrix(m)
+			self.isValid = False
+			if(self.theta == 90 or self.theta == -270):
+				if(self.maze[int(self.x)][int(self.z-1)] == 0):
+					self.isValid = True
+			elif(self.theta == 0):
+				if(self.maze[int(self.x-1)][int(self.z)] == 0):
+					self.isValid = True
+			elif(self.theta == 270 or self.theta == -90):
+				if(self.maze[int(self.x-2)][int(self.z-1)] == 0):
+					self.isValid = True
+			elif(self.theta == 180 or self.theta == -180):
+				if(self.maze[int(self.x-1)][int(self.z-2)] == 0):
+					self.isValid = True										
+			if(self.isValid):
+				m = viz.Matrix()
+				self.moveZ = math.cos(math.radians(self.theta))
+				self.moveX = math.sin(math.radians(self.theta))				
+				self.x = self.x + self.moveX
+				self.z = self.z + self.moveZ				
+				m.postAxisAngle(0,1,0,self.theta)
+				m.postTrans(self.x,self.y,self.z)
+				self.avatar.setMatrix(m)
+
 			
-			print(self.x)
-			print(self.z)
 		if (key == '1'):
 			view = viz.MainView
 			mat = viz.Matrix()
@@ -171,16 +215,17 @@ class Maze(viz.EventClass):
 			view.setMatrix(mat)
 			self.first = False
 			self.bird = False
+			
 		if (key == '2'):
 			view = viz.MainView
 			mat = viz.Matrix()
 			mat.postAxisAngle(0,1,0,270)
-			
 			mat.postAxisAngle(0,0,1,40)
 			mat.postTrans(self.birdX,self.birdY,self.birdZ)
 			view.setMatrix(mat)
 			self.first = False
 			self.bird = True
+			
 		if (key == '3' or self.first):
 			view = viz.MainView
 			mat = viz.Matrix()
@@ -189,6 +234,7 @@ class Maze(viz.EventClass):
 			view.setMatrix(mat)
 			self.first = True
 			self.bird = False
+			
 		if (self.bird == True):
 			if (key == 'a'):
 				self.birdZ = self.birdZ -1
@@ -198,6 +244,7 @@ class Maze(viz.EventClass):
 				mat.postAxisAngle(0,0,1,40)
 				mat.postTrans(self.birdX,self.birdY,self.birdZ)
 				view.setMatrix(mat)
+				
 			elif (key == 'd'):
 				view = viz.MainView
 				self.birdZ = self.birdZ + 1
@@ -207,6 +254,7 @@ class Maze(viz.EventClass):
 				mat.postTrans(self.birdX,self.birdY,self.birdZ)
 				view.setMatrix(mat)
 				print(self.birdZ)
+				
 			elif (key == 'w'):
 				view = viz.MainView
 				self.birdX = self.birdX - 1
@@ -216,6 +264,7 @@ class Maze(viz.EventClass):
 				mat.postTrans(self.birdX,self.birdY,self.birdZ)
 				view.setMatrix(mat)
 				print(self.birdX)
+				
 			elif (key == 's'):
 				view = viz.MainView
 				self.birdX = self.birdX +1
